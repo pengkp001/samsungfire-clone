5f5f2c15dca5f5e04a6830e31172fb6dd0da1431
pengkp001 <gusdud0325@naver.com>
2025-06-02 13:07:00 +0900

    헤더 고정 기능, 메인 배너 슬라이드

--- js/swCard.js
+++ js/swCard.js
@@ -1,4 +1,4 @@
-const swCard = new Swiper(".sw-card", {
+var swCard = new Swiper(".sw-card", {
   slidesPerView: 3,
   spaceBetween: 30,
   pagination: {
---
+++ samsungfire-clone/README.md
@@ -0,0 +1,9 @@
+# 삼성화재 클론 코딩
+
+- https://www.samsungfire.com/
+
+## 반응형 break point
+
+- max-width: 1219px
+- max-width: 767px
+- max-width: 585px
